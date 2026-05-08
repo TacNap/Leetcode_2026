ec32f27b70e2836c9847de92d6d3945758435bd8
Matthew <matthewtlittle@gmail.com>
2026-05-08 19:26:47 +1000

    added strings

--- leetcode_2026.App/Notes/Arrays.cs
+++ leetcode_2026.App/Notes/Arrays.cs
@@ -9,12 +9,7 @@ public class ArrayNotes
         Console.WriteLine("### Copying Arrays Into Themselves ###");
         // Declare and instantiate a new string[]
         string[] strs = { "one", "two", "three", "four", "five" };
-        Console.WriteLine("Array Before Changes:");
-        foreach (var str in strs)
-        {
-            Console.Write($"{str}, ");
-        }
-        Console.WriteLine();
+        Console.WriteLine("Array Before Changes: " + string.Join(", ", strs));
 
         // Resize the array to give it one more element
         Array.Resize(ref strs, strs.Length + 1);
@@ -26,10 +21,18 @@ public class ArrayNotes
         /// Copy a total of 3 elements
         Array.Copy(strs, 2, strs, 3, 3);
         strs[2] = "NEW";
-        Console.WriteLine("Array After Changes:");
-        foreach (var str in strs)
-        {
-            Console.Write($"{str}, ");
-        }
+        Console.WriteLine("Array After Changes: " + string.Join(", ", strs));
+    }
+
+    public void Methods()
+    {
+        // Finding the index of an item
+        string[] tings = { "dragon dagger", "bitcoin", "Hawkmoon", "Plasma Pistol" };
+        int hawkmoonIndex = Array.IndexOf(tings, "Hawkmoon");
+        Console.WriteLine($"{tings[hawkmoonIndex]}");
+
+        // Resizing an array - requires the ref keyword
+        Array.Resize(ref tings, tings.Length + 1);
+        tings[^1] = "Supercharged"; // Accesses the last element of the array
     }
 }
---
+++ leetcode_2026.App/Notes/Strings.cs
@@ -0,0 +1,8 @@
+namespace leetcode_2026.App.Problems.Arrays;
+
+/// <summary>
+/// </summary>
+public class StringNotes
+{
+    public void Copying() { }
+}
